45b03eab01690f383043e347844926b8049e6b60
janlingen <82512348+janlingen@users.noreply.github.com>
2022-07-09 14:02:20 +0200

    Fix type, Adapt naming conventions

---
+++ javascript/hello-world/hello-world.js
@@ -0,0 +1,7 @@
+console.log("Hello World")
+
+var name = "Jan"
+var age = 12
+var decimal = 12.0123213
+
+console.log("My name is " + name + " Age: " + age + " Decimal: " + decimal )
--- javascript/hello-world/hellow_world.js
+++
@@ -1,5 +0,0 @@
-console.log("Hello World")
-
-var name = "jan"
-var age = 12
-var decimal = 12.0123213
