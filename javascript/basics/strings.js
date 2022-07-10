6ede81849960928ff332c21978862f4bc89fb2ef
janlingen <82512348+janlingen@users.noreply.github.com>
2022-07-10 14:11:54 +0200

    Trying out strings

---
+++ javascript/basics/strings.js
@@ -0,0 +1,11 @@
+var githubName = "janlingen"
+console.log(typeof githubName)
+console.log(githubName)
+console.log(githubName.length)
+console.log(githubName.toUpperCase())
+console.log(githubName.substring(0,3))
+
+var a = "a"
+var b = "b"
+console.log(a+b)
+console.log(`${a} ${b.length}`)
