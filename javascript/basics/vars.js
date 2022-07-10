f3768b2c1cbcc094c7117fd7f90bf2e5ec0a9f3f
janlingen <82512348+janlingen@users.noreply.github.com>
2022-07-10 14:12:25 +0200

    Testings around with vars

---
+++ javascript/basics/vars.js
@@ -0,0 +1,23 @@
+var stringType = "hello"
+console.log(stringType)
+console.log(typeof(stringType))
+
+var numberType1 =  12.2
+console.log(numberType1)
+console.log(typeof(numberType1))
+
+var numberType2 = 12 
+console.log(numberType2)
+console.log(typeof(numberType2))
+
+var booleanType = true
+console.log(booleanType)
+console.log(typeof(booleanType))
+
+var dateType = new Date(2022,11,25)
+console.log(dateType)
+console.log(typeof(dateType))
+
+var objectCar = {} // empty object
+
+var empty = undefined // not initialized
